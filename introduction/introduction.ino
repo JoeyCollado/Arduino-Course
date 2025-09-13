718df6103b9c6d8f3b0eb895eb70c83e27b5ba85
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-13 09:17:39 +0800

    intro

---
+++ introduction/introduction.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//board port (arduino uno) communicates with the board  
+//we need to insert a cable to enable the port connection
+//program = sketch
+//upload = is when we deploy our code to the machine to work
+
+//programming -> compile -> upload
