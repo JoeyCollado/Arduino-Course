dc60c175e2ffe9b9c166b7486a53d1369c7c949e
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-14 16:19:14 +0800

    hello world

---
+++ hello_world/hello_world.ino
@@ -0,0 +1,14 @@
+//first program
+
+void setup() { //codes written inside here will only run once
+  // put your setup code here, to run once:
+  Serial.begin(9600); //sets the data rate in bits per second (baud) for serial data transmission
+  Serial.println("Hello World"); //print hello world when the arduino and computer communicate 
+}
+
+void loop() { //codes written here will run repeatedly
+  // put your main code here, to run repeatedly:
+
+}
+
+// ; = semi colon is mandatory to end sentences
