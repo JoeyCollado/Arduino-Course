0a07330fe59e6ea04400575b40197bf1133381af
Joey Collado <akosijoeyaibertcollado@gmail.com>
2025-09-22 19:04:31 +0800

    motor

---
+++ page.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default page
+
+
+//prisma setup
+//npm install prisma @prisma/client
+//npx prisma init
+
+//postgre url
+//https://console.neon.tech/app/projects/dry-sun-36071277?database=neondb
+//https://console.neon.tech/app/org-restless-cell-67537617/welcome
+
